809b5a325f93e81f73faabf79c780e91076f3101
AmalSalahzekry <amalsalahzekri@gmail.com>
2023-09-19 12:06:33 -0700

    pritn f

--- _printf.c
+++ _printf.c
@@ -58,50 +58,49 @@ static int print_int(int n)
  * @format: The format string
  * Return: The number of characters printed
  */
+
 int _printf(const char *format, ...)
 {
-va_list args;
-int printed_chars = 0;
-int num = 0;
+    va_list args;
+    int printed_chars = 0;
 
-va_start(args, format);
+    va_start(args, format);
+    while (*format)
+    {
+        if (*format != '%')
+        {
+            printed_chars += print_char(*format);
+        }
+        else
+        {
+            format++;
+            switch (*format)
+            {
+                case 'c':
+                    printed_chars += print_char(va_arg(args, int));
+                    break;
+                case 's':
+                    printed_chars += print_str(va_arg(args, char *));
+                    break;
+                case '%':
+                    printed_chars += print_char('%');
+                    break;
+                case 'i':
+                    printed_chars += print_int(va_arg(args, int));
+                    break;
+                default:
+                    printed_chars += print_char('%');
+                    if (*format)
+                    {
+                        printed_chars += print_char(*format);
+                    }
+                    break;
+            }
+        }
+        format++;
+    }
 
-while (*format)
-{
-if (*format != '%')
-{
-printed_chars += print_char(*format);
-}
-else
-{
-format++;
-switch (*format)
-{
-case 'c':
-printed_chars += print_char(va_arg(args, int));
-break;
-case 's':
-printed_chars += print_str(va_arg(args, char *));
-break;
-case '%':
-printed_chars += print_char('%');
-break;
-case 'i':
-int num = va_arg(args, int);
-printed_chars += print_int(num);
-break;
-default:
-printed_chars += print_char('%');
-if (*format)
-{
-printed_chars += print_char(*format);
-}
-}
-}
-format++;
-}
+    va_end(args);
 
-va_end(args);
-
-return (printed_chars);
+    return printed_chars;
 }
