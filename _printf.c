fadd7884a51f4ef6d3ad471ad946580fc403d951
AmalSalahzekry <133532839+AmalSalahzekry@users.noreply.github.com>
2023-09-20 01:22:51 +0300

    Update _printf.c

--- _printf.c
+++ _printf.c
@@ -61,7 +61,6 @@ int _printf(const char *format, ...)
 {
 	va_list args;
 	int printed_chars = 0;
-	
 	va_start(args, format);
 
 	while (*format)
@@ -71,10 +70,8 @@ int _printf(const char *format, ...)
 		else
 		{
 			format++;
-			
 			if (*format == 'c') 
 				printed_chars += print_char(va_arg(args, int));
-			
 			else if (*format == 's') 
 				printed_chars += print_str(va_arg(args, char *));
 			else if (*format == '%') 
