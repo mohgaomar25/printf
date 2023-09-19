38705d70bae445020ee1594a5e7516914359937b
AmalSalahzekry <amalsalahzekri@gmail.com>
2023-09-19 11:18:39 -0700

    edite printf

--- _printf.c
+++ _printf.c
@@ -3,6 +3,33 @@
 #include <unistd.h>
 
 /**
+ * print_char - Prints a single character to stdout
+ * @c: The character to be printed
+ * Return: The number of characters printed
+ */
+static int print_char(int c)
+{
+        return (write(1, &c, 1));
+}
+
+/**
+ * print_str - Prints a string to stdout
+ * @str: The string to be printed
+ * Return: The number of characters printed
+ */
+static int print_str(const char *str)
+{
+        int printed_chars = 0;
+
+        while (*str)
+        {
+                printed_chars += write(1, str, 1);
+                str++;
+        }
+        return (printed_chars);
+}
+
+/**
  * _printf - Custom printf function
  * @format: The format string
  * Return: The number of characters printed
@@ -37,7 +64,6 @@ printed_chars += print_char('%');
 break;
 case 'i':
 int num = va_arg(args, int);
-/* You can use a separate function to print integers*/
 printed_chars += print_int(num);
 break;
 default:
