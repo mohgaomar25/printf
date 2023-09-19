ddf5aeb220bb919728dee7c32e9de223bca71b38
mohgaomar25 <138797863+mohgaomar25@users.noreply.github.com>
2023-09-20 01:08:25 +0300

    Update _printf.c

--- _printf.c
+++ _printf.c
@@ -61,47 +61,32 @@ int _printf(const char *format, ...)
 {
 	va_list args;
 	int printed_chars = 0;
-
 	va_start(args, format);
 
 	while (*format)
 	{
 		if (*format != '%')
-		{
 			printed_chars += print_char(*format);
-		}
 		else
 		{
 			format++;
 			if (*format == 'c') 
-			{
 				printed_chars += print_char(va_arg(args, int));
-			}
 			else if (*format == 's') 
-			{
 				printed_chars += print_str(va_arg(args, char *));
-			}
-				/*
 			else if (*format == '%') 
-			{
 				printed_chars += print_char('%');
-			}*/
 			else if (*format == 'd' || *format == 'i') 
-			{ 
 				printed_chars += print_int(va_arg(args, int));
-			}
 			else 
 			{
 				printed_chars += print_char('%');
-			      if (*format) 
-			      { 
+			      if (*format)
 				      printed_chars += print_char(*format);
-			      }
 			}
 		}
 		format++; 
 	}
-
 	va_end(args);
 
 	return (printed_chars);
