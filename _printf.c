f9739baa6d6088c60aafadc2bc24f4b08c5ab8d3
AmalSalahzekry <amalsalahzekri@gmail.com>
2023-09-19 13:21:53 -0700

    my fun

--- _printf.c
+++ _printf.c
@@ -29,6 +29,29 @@ static int print_str(const char *str)
 }
 
 /**
+ * print_int - Prints an integer to stdout
+ * @n: The integer to be printed
+ * Return: The number of characters printed
+ */
+static int print_int(int n)
+{
+    int printed_chars = 0;
+
+    if (n < 0)
+    {
+        printed_chars += print_char('-');
+        n = -n;
+    }
+    if (n / 10)
+    {
+        printed_chars += print_int(n / 10);
+    }
+    printed_chars += print_char((n % 10) + '0');
+
+    return printed_chars;
+}
+
+/**
  * _printf - Custom printf function
  * @format: The format string
  * Return: The number of characters printed
