f8a3437b2a9d6abf5439e1a52f9da9fde30f284c
mohgaomar25 <138797863+mohgaomar25@users.noreply.github.com>
2023-09-19 22:58:18 +0300

    Update _printf.c

--- _printf.c
+++ _printf.c
@@ -9,7 +9,7 @@
  */
 static int print_char(int c)
 {
-return (write(1, &c, 1));
+  return (write(1, &c, 1));
 }
 
 /**
@@ -19,13 +19,13 @@ return (write(1, &c, 1));
  */
 static int print_str(const char *str)
 {
-int printed_chars = 0;
-while (*str)
-{
-printed_chars += write(1, str, 1);
-str++;
-}
-return (printed_chars);
+  int printed_chars = 0;
+  while (*str)
+  {
+    printed_chars += write(1, str, 1);
+    str++;
+  }
+  return (printed_chars);
 }
 
 /**
@@ -35,43 +35,47 @@ return (printed_chars);
  */
 int _printf(const char *format, ...)
 {
-va_list args;
-int printed_chars = 0;
+  va_list args;
+  int printed_chars = 0;
 
-va_start(args, format);
+  va_start(args, format);
 
-while (*format)
-{
-if (*format != '%')
-{
-printed_chars += print_char(*format);
-}
-else
-{
-format++;
-switch (*format)
-{
-case 'c':
-printed_chars += print_char(va_arg(args, int));
-break;
-case 's':
-printed_chars += print_str(va_arg(args, char *));
-break;
-case '%':
-printed_chars += print_char('%');
-break;
-default:
-printed_chars += print_char('%');
-if (*format)
-{
-printed_chars += print_char(*format);
-}
-}
-}
-format++;
-}
+  while (*format)
+  {
+    if (*format != '%')
+    {
+      printed_chars += print_char(*format);
+    }
+    else
+    {
+      format++;
+      switch (*format)
+      {
+        case 'c':
+          printed_chars += print_char(va_arg(args, int));
+          break;
+        case 's':
+          printed_chars += print_str(va_arg(args, char *));
+          break;
+        case '%':
+          printed_chars += print_char('%');
+          break;
+        case 'i':
+          int num = va_arg(args, int);
+          printed_chars += print_int(num);
+          break;
+        default:
+          printed_chars += print_char('%');
+          if (*format)
+          {
+            printed_chars += print_char(*format);
+          }
+      }
+    }
+    format++;
+  }
 
-va_end(args);
+  va_end(args);
 
-return (printed_chars);
+  return (printed_chars);
 }
