1abec00dcd595496d79080f3590d0ca92fa78ac9
AmalSalahzekry <amalsalahzekri@gmail.com>
2023-09-19 10:41:02 -0700

    repush

--- print_int.c
+++ print_int.c
@@ -11,14 +11,12 @@ static int print_int(int n)
 {
 	int printed_chars = 0;
 
- /* Handle negative numbers*/
 	if (n < 0)
 	{
 		printed_chars += print_char('-');
 		n = -n;
 	}
 
-	/* Handle digits one by one*/
 	if (n / 10)
 	{
 		printed_chars += print_int(n / 10);
