c32b78b7cbacab777312c74902483534301f1ecb
mohgaomar25 <138797863+mohgaomar25@users.noreply.github.com>
2023-09-19 22:50:32 +0300

    Update _printf.c

--- _printf.c
+++ _printf.c
@@ -1,7 +1,6 @@
 #include "main.h"
 #include <stdarg.h>
 #include <unistd.h>
-#include <string.h>
 
 /**
  * print_char - Prints a single character to stdout
@@ -10,7 +9,7 @@
  */
 static int print_char(int c)
 {
-	return (write(1, &c, 1));
+return (write(1, &c, 1));
 }
 
 /**
@@ -20,39 +19,14 @@ static int print_char(int c)
  */
 static int print_str(const char *str)
 {
-	int printed_chars = 0;
-
-	while (*str)
-	{
-		printed_chars += write(1, str, 1);
-		str++;
-	}
-	return (printed_chars);
-}
-
-/**
- * print_int - Prints an integer to stdout
- * @n: The integer to be printed
- * Return: The number of characters printed
- 
-static int print_int(int n)
+int printed_chars = 0;
+while (*str)
 {
-	int printed_chars = 0;
-
-	if (n < 0)
-	{
-		printed_chars += print_char('-');
-		n = -n;
-	}
-	if (n / 10)
-	{
-		printed_chars += print_int(n / 10);
-	}
-	printed_chars += print_char((n % 10) + '0');
-
-	return (printed_chars);
+printed_chars += write(1, str, 1);
+str++;
 }
-*/
+return (printed_chars);
+}
 
 /**
  * _printf - Custom printf function
@@ -61,50 +35,43 @@ static int print_int(int n)
  */
 int _printf(const char *format, ...)
 {
-	va_list args;
-	int printed_chars = 0;
+va_list args;
+int printed_chars = 0;
 
-	va_start(args, format);
+va_start(args, format);
 
-	while (*format)
-	{
-		if (*format != '%')
-		{
-			printed_chars += print_char(*format);
-		}
-		else
-		{
-			format++;
-			switch (*format)
-			{
-				case 'c':
-					printed_chars += print_char(va_arg(args, int));
-					break;
-				case 's':
-					printed_chars += print_str(va_arg(args, char *));
-					break;
-				case '%':
-					printed_chars += print_char('%');
-					break;
-				case 'd':
-				case 'i':
-					/* Handle 'd' and 'i' specifiers */
-					int num = va_arg(args, int);
-					/* You can use a separate function to print integers */
-					printed_chars += print_int(num);
-					break;
-				default:
-					printed_chars += print_char('%');
-					if (*format)
-					{
-						printed_chars += print_char(*format);
-					}
-			}
-		}
-		format++;
-	}
-	
-	va_end(args);
+while (*format)
+{
+if (*format != '%')
+{
+printed_chars += print_char(*format);
+}
+else
+{
+format++;
+switch (*format)
+{
+case 'c':
+printed_chars += print_char(va_arg(args, int));
+break;
+case 's':
+printed_chars += print_str(va_arg(args, char *));
+break;
+case '%':
+printed_chars += print_char('%');
+break;
+default:
+printed_chars += print_char('%');
+if (*format)
+{
+printed_chars += print_char(*format);
+}
+}
+}
+format++;
+}
 
-	return (printed_chars);
+va_end(args);
+
+return (printed_chars);
 }
