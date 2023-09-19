0e4ca3282f3d2ef4152881af06b61260b3ca047e
AmalSalahzekry <amalsalahzekri@gmail.com>
2023-09-19 11:49:42 -0700

    m

--- main.h
+++ main.h
@@ -3,10 +3,9 @@
 
 #include <stdarg.h>
 
-int _printf(const char *format, ...);
 static int print_char(int c);
 static int print_str(const char *str);
 static int print_int(int n);
-
+int _printf(const char *format, ...);
 
 #endif /* MAIN_H */
