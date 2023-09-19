2766ec3bc15d8fe7d7d3bbe7302ef4d0d7401440
mmohgaomar25 <mohgaomar25@gmail.com>
2023-09-19 07:01:22 -0700

    fix

--- _printf.c
+++ _printf.c
@@ -3,58 +3,66 @@
 #include <unistd.h>
 
 /**
- * _printf - Custom printf function
- * @format: The format string
+ * _printf - Produces output according to a format.
+ * @format: A character string containing format specifiers.
  *
- * Return: The number of characters printed (excluding null byte)
+ * Return: The number of characters printed (excluding the null byte).
  */
 int _printf(const char *format, ...)
 {
-	va_list args;
-	int count = 0;
-	char c;
-	char *str;
+va_list args;
+int printed_chars = 0;
+char c;
+char *str;
 
-	va_start(args, format);
+va_start(args, format);
 
-	while (format && *format)
-	{
-		if (*format != '%')
-		{
-			write(1, format, 1);
-			count++;
-		}
-		else
-		{
-			format++;
-			if (*format == 'c')
-			{
-				c = va_arg(args, int);
-				write(1, &c, 1);
-				count++;
-			}
-			else if (*format == 's')
-			{
-				str = va_arg(args, char *);
-				if (!str)
-					str = "(null)";
-				while (*str)
-				{
-					write(1, str, 1);
-					str++;
-					count++;
-				}
-			}
-			else if (*format == '%')
-			{
-				write(1, "%", 1);
-				count++;
-			}
-		}
-		format++;
-	}
+while (*format)
+{
+if (*format != '%')
+{
+write(1, format, 1); /* Print the character */
+printed_chars++;
+}
+else
+{
+format++; /* Move past '%' */
+switch (*format)
+{
+case 'c':
+c = va_arg(args, int);
+write(1, &c, 1);
+printed_chars++;
+break;
+case 's':
+str = va_arg(args, char *);
+if (str == NULL)
+str = "(null)";
+while (*str)
+{
+write(1, str, 1);
+str++;
+printed_chars++;
+}
+break;
+case '%':
+write(1, "%", 1);
+printed_chars++;
+break;
+default:
+write(1, "%", 1); /* Print '%' as default */
+printed_chars++;
+if (*format)
+{
+write(1, format, 1); /* Print the unknown format specifier */
+printed_chars++;
+}
+}
+}
+format++; /* Move to the next character in the format string */
+}
 
-	va_end(args);
+va_end(args);
 
-	return (count);
+return (printed_chars);
 }
